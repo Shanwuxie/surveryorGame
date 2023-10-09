3771e9fb1f9ef73b19b95ae33b15fae7a5597a80
Shanwuxie <116215009+Shanwuxie@users.noreply.github.com>
2023-10-08 23:11:59 -0400

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -47,7 +47,7 @@ public class Inventory extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
